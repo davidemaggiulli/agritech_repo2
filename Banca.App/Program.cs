fb3c4092172e0919d6e9dccee7f2f7914402f18a
Davide Maggiulli <davide.maggiulli@gmail.com>
2022-06-23 15:29:19 +0200

    Creazione progetto Banca

---
+++ Banca.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Banca.App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
